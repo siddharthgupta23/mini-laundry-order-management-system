b4c12efdf849b3280dad80520e4149e3d0387471
siddharthgupta23 <siddharthgupta1603@gmail.com>
2026-05-01 17:41:54 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:8080",
     credentials: true,
   })
 );
